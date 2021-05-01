d29870ec13b3d9ea350d10a56f184786ae3e6554
Aritra Sur Roy <aritra1997@hotmail.com>
2021-05-01 11:19:28 +0530

    Add project files.

---
+++ SodokuSolver/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SodokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = new int[9,9] { 
+                                        { 5, 3, 4, 6, 7, 8, 9, 1, 2 }, 
+                                        { 6, 7, 2, 1, 9, 5, 3, 4, 0 }, 
+                                        { 1, 0, 8, 3, 4, 2, 5, 6, 7 }, 
+                                        { 8, 5, 9, 0, 0, 1, 4, 2, 3 }, 
+                                        { 4, 2, 6, 0, 5, 3, 7, 9, 1 }, 
+                                        { 7, 1, 3, 9, 2, 4, 8, 5, 6 }, 
+                                        { 9, 0, 1, 5, 3, 7, 2, 8, 4 }, 
+                                        { 2, 8, 7, 4, 1, 9, 0, 3, 5 }, 
+                                        { 3, 4, 5, 2, 8, 6, 1, 7, 0 } 
+                                    };
+            SodukuSolver sodukuSolver = new SodukuSolver(board);
+            sodukuSolver.Solve();
+            sodukuSolver.PrintSolution();
+        }
+    }
+}
---
+++ SodokuSolver/SodukuSolver.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace SodokuSolver
+{
+    internal class SodukuSolver
+    {
+        private const int BOARDSIZE = 9;
+        private readonly int[,] BOARD;
+
+        public SodukuSolver(int[,] board)
+        {
+            BOARD = board;
+        }
+
+        public bool Solve()
+        {
+            // find blank spot
+            int i = 0, j = 0;
+            bool foundBlankSpot = false;
+
+            while(i < BOARDSIZE)
+            {
+                j = 0;
+                while(j < BOARDSIZE)
+                {
+                    if (BOARD[i,j] == 0)
+                    {
+                        foundBlankSpot = true;
+                        break;
+                    }
+                    j++;
+                }
+                
+                if(foundBlankSpot)
+                {
+                    break;
+                }
+                i++;
+            }
+
+            if(!foundBlankSpot)
+            {
+                return CheckBoard();
+            }
+
+            // put a number there from 1 to 9
+            for(int num = 1;num <= 9;num++)
+            {
+                BOARD[i,j] = num;
+                if(Solve())
+                {
+                    return true;
+                }
+                BOARD[i,j] = 0;
+            }
+
+            return false;
+        }
+
+        private bool CheckBoard()
+        {
+            // check each row
+            for(int i = 0;i < BOARDSIZE;i++)
+            {
+                int[] dict = new int[10];
+                for (int j = 0;j < BOARDSIZE;j++)
+                {
+                    dict[BOARD[i,j]]++;
+                    if(dict[BOARD[i,j]] > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // check each column
+            for (int i = 0;i < BOARDSIZE;i++)
+            {
+                int[] dict = new int[10];
+                for (int j = 0;j < BOARDSIZE;j++)
+                {
+                    dict[BOARD[j,i]]++;
+                    if (dict[BOARD[j,i]] > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // check each 3x3 boxes
+            for(int i = 0;i < 3;i++)
+            {
+                for(int j = 0;j < 3;j++)
+                {
+                    int[] dict = new int[10];
+                    for(int x = 0;x < 3;x++)
+                    {
+                        for(int y = 0;y < 3;y++)
+                        {
+                            int i_idx = (x + (i * 3)), j_idx = (y + (j * 3));
+                            dict[BOARD[i_idx,j_idx]]++;
+                            if(dict[BOARD[i_idx,j_idx]] > 1)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void PrintSolution()
+        {
+            for (int i = 0; i < BOARDSIZE; i++)
+            {
+                for (int j = 0; j < BOARDSIZE; j++)
+                {
+                    Console.Write($"{BOARD[i,j]},");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
